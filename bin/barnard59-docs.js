683d3cc68f797b5e5503ddb5b01ba6d6992d96aa
magdalena@surowka.ch <magdalena@surowka.ch>
2021-02-16 15:59:56 +0100

    1: doesn't fail if  packages are missing manifest

--- bin/barnard59-docs.js
+++ bin/barnard59-docs.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import chalk from 'chalk'
 import { Command } from 'commander/esm.mjs'
 import { infoToMarkdown, manifestToMarkdown, packageInfo, packageManifest } from '../index.js'
 
@@ -14,13 +14,21 @@ program.parse(process.argv)
 const output = []
 
 for (const name of program.args) {
-  const info = await packageInfo(name)
-  const manifest = await packageManifest(name)
+
+
+  let info, manifest
+  try {
+    info = await packageInfo(name)
+    manifest = await packageManifest(name)
+  } catch (err) {
+    console.warn(chalk.yellow(`Generating doc for ${name} failed. This packaged will be skipped. Error trace:`))
+    console.warn(err)
+    continue
+  }
 
   output.push(infoToMarkdown(info, program.opts()))
   output.push(manifestToMarkdown(manifest, program.opts()))
 }
 
 const markdown = output.join('\n')
-
 process.stdout.write(markdown)
